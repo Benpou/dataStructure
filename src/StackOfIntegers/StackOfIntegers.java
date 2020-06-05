97ef3c8610fc0a6ec8dc8dccd1071f665d0daa8b
ben <behnam.pournasr@farmersinsurance.com>
2020-06-05 14:41:56 -0700

    stackOfInteger
    Signed-off-by: ben <behnam.pournasr@farmersinsurance.com>

---
+++ src/StackOfIntegers/StackOfIntegers.java
@@ -0,0 +1,43 @@
+package StackOfIntegers;
+
+public class StackOfIntegers {
+    private int[] elements;
+    private int size;
+    public static final int DEFAULT_CAPACITY = 16;
+
+    public StackOfIntegers(){
+        this (DEFAULT_CAPACITY);
+    }
+
+    public StackOfIntegers(int capacity){
+        elements= new int[capacity];
+    }
+
+    public void push(int value) {
+        if (size >= elements.length) {
+            int[] temp = new int[elements.length * 2];
+            System.arraycopy(elements, 0, temp, 0,elements.length);
+            elements = temp;
+        }
+        elements[size++] = value;
+    }
+
+    //Return and remove the top element from the stack
+    public int pop() {
+        return elements[--size];
+    }
+
+    //Return the top element from the stack
+    public int peek(){
+        return elements[size - 1];
+    }
+
+    public boolean empty(){
+        return size == 0;
+    }
+
+    //Return the number of elements the stack
+    public int getSize(){
+        return size;
+    }
+}
