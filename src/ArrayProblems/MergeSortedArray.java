ba23ed40a8a794bb7d5d43affa152f2b507a1b63
benpou2012 <benpou2012@gmail.com>
2021-01-04 20:42:37 -0800

    merge sorted array
    Signed-off-by: benpou2012 <benpou2012@gmail.com>

--- src/ArrayProblems/MergeSortedArray.java
+++ src/ArrayProblems/MergeSortedArray.java
@@ -2,31 +2,68 @@ package ArrayProblems;
 
 public class MergeSortedArray {
 
-  // Time complexity: O(n1+n2)
+  // Time complexity: O(n+m)
   // space : O(n1 + n2)
   public static void main(String[] args) {
 
-    int[] arr1 = {1,2,3, 0 ,0,0};
+    int[] arr1 = {1, 10, 0 ,0,0};
     int[] arr2= {2,5,6};
-    int n = 3;
+    int n = 2;
     int m = 3;
     int[] arr3 = new int[n + m];
 
-    solution1(arr1,n, arr2,m, arr3);
+    //solution1(arr1,n, arr2,m, arr3);
+    
+    solution2(arr1, n, arr2, m);
 
     System.out.print("Array after merging:");
 
-    for(int i=0; i < arr3.length; i++) {
-      System.out.print(arr3[i] + " ");
+//    for(int i=0; i < arr3.length; i++) {
+//      System.out.print(arr3[i] + " ");
+//    }
+    for(int i=0; i < arr1.length; i++) {
+      System.out.print(arr1[i] + " ");
     }
 
   }
 
 
-  public static void soluion2(int[] nums1, int n, int[] nums2, int m, int[] arr3){
+  public static void solution2(int[] nums1, int m, int[] nums2, int n){
 
+    int runner1 = m - 1;
+    int runner2 = n - 1;
+    int resultRunner = m + n - 1;
 
+    /*
+     * Filling from the right, because if we would
+     * do that from the left, we would need to shift
+     * elements in the nums1.
+     */
+    while (runner1 >= 0 && runner2 >= 0) {
+      if (nums1[runner1] > nums2[runner2]) {
+        nums1[resultRunner] = nums1[runner1];
 
+        runner1--;
+      } else {
+        nums1[resultRunner] = nums2[runner2];
+
+        runner2--;
+      }
+
+      resultRunner--;
+    }
+
+    /*
+     * We only concerned about nums2 now, because
+     * if there are some unprocessed elements in nums1,
+     * they are already at their correct places.
+     */
+    while (runner2 >= 0) {
+      nums1[resultRunner] = nums2[runner2];
+
+      runner2--;
+      resultRunner--;
+    }
   }
 
 
