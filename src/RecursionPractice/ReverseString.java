5bbaa34a5520db354595bd7d186cc8881d157a59
benpou2012 <benpou2012@gmail.com>
2021-01-05 16:13:02 -0800

    recursion char array
    Signed-off-by: benpou2012 <benpou2012@gmail.com>

---
+++ src/RecursionPractice/ReverseString.java
@@ -0,0 +1,24 @@
+package RecursionPractice;
+
+public class ReverseString {
+
+  public static void main(String[] args) {
+
+    char[] s = {'h','e','l','l','o'};
+
+  }
+
+  public static void helper(char[] s, int left, int right) {
+    if (left >= right) return;
+
+    char temp = s[left];
+    s[left++] = s[right];
+    s[right--] = temp;
+    helper(s, left, right);
+  }
+
+  public static void reverseString(char[] s) {
+    helper(s, 0, s.length -1);
+  }
+
+}
