69d976d693b72363adf1428af87129469981b025
benpou2012 <benpou2012@gmail.com>
2021-02-13 11:50:25 -0800

    update branch
    Signed-off-by: benpou2012 <benpou2012@gmail.com>

--- src/ArrayProblems/SearchLinear.java
+++ src/ArrayProblems/SearchLinear.java
@@ -9,6 +9,25 @@ public class SearchLinear {
     System.out.println(solution(arr));
 
   }
+  // while loop solution:
+  public static boolean solution1(int[] arr) {
+    int N = arr.length;
+    int i = 0;
+
+    // walk up
+    while (i+1 < N && arr[i] < arr[i+1])
+      i++;
+
+    // peak can't be first or last
+    if (i == 0 || i == N-1)
+      return false;
+
+    // walk down
+    while (i+1 < N && arr[i] > arr[i+1])
+      i++;
+
+    return i == N-1;
+  }
 
   // check if arr is mountain arr
   public static boolean solution(int[] arr) {
