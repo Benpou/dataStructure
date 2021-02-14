d3c12e9a3fb2a03091c567349e8da7aaf0ced074
benpou2012 <benpou2012@gmail.com>
2021-02-14 09:39:16 -0800

    greatest element in the right
    Signed-off-by: benpou2012 <benpou2012@gmail.com>

--- src/ArrayProblems/GreatestElementInTheRight.java
+++ src/ArrayProblems/GreatestElementInTheRight.java
@@ -1,4 +1,28 @@
 package ArrayProblems;
 
 public class GreatestElementInTheRight {
+
+  public static void main(String[] args) {
+
+    int [] arr = {17,18,4,5,6,1,7};
+
+int [] x = replaceElements(arr);
+    for( int i =0; i < arr.length; i++) {
+
+      System.out.println( x[i]);
+
+    }
+  }
+
+  public static int[] replaceElements(int[] arr) {
+    int temp =0;
+    int max = -1;
+    int len = arr.length;
+    for (int i = arr.length - 1; i >= 0; i--) {
+       temp = arr[i];
+        arr[i] = max;
+        max = Math.max(max, temp);
+    }
+    return arr;
+  }
 }
